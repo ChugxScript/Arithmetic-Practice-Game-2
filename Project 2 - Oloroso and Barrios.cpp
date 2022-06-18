34fa106d95424e9f877ebf1c9dd26d34350d36fd
Sluhii <101156843+Sluhii@users.noreply.github.com>
2022-06-18 14:11:31 +0800

    depa kumpleto HAHA

--- Project 2 - Oloroso and Barrios.cpp
+++ Project 2 - Oloroso and Barrios.cpp
@@ -1,5 +1,5 @@
-/*Quiz 3 - Previous Machine Problem in CPP
-by Andrew R. Oloroso*/
+/*Project 2 in CPP
+by Andrew R. Oloroso and Armand Angelo C. Barrios*/
 #include <iostream>
 #include <iomanip> //for setprecision
 #include <fstream>
@@ -22,11 +22,12 @@ typedef struct node { //self-referecial structre
 class MyClass {
 private:
     NODE *head;
+    int locate(string n);
     bool isEmpty();
 public:
     void init();
-    void add(string n, int a1, int a2, int a3);
-    void del(string n);
+    void logIn();
+    void leaderboard();
     void display();
     void save();
     void retrieve();
@@ -36,64 +37,74 @@ void MyClass::init(){
     head=NULL;
 }
 
-int menu(){
-    int num;
-    system("cls");
-    cout <<"MENU"<<endl;
-    cout <<"[1] Add Record"<<endl;
-    cout <<"[2] Delete Record"<<endl;
-    cout <<"[3] Display"<<endl;
-    cout <<"[4] Save and Exit"<<endl;
-    cout <<"Select(1-4): ";
-    cin >> num;
-    return num;
-}
-
-void MyClass::add(string n, int a1, int a2, int a3){
+void MyClass::logIn(){
     NODE *p, *q, *newNode;
-    p=q=head;
-    newNode = new NODE;
-    newNode->name = n;
-    newNode->q1 = a1; newNode->q2 = a2; newNode->q3 = a3;
-    while (p!=NULL && p->name<n){
-        q = p;
-        p = p->next;
-    }
-    if (head==p){
-        head = newNode;//first element
-    }else{
-        q->next = newNode;
-    }newNode->next = p;
+    p=q=head; newNode = new NODE;
+    cout <<"Enter Username/UserID: "; cin.ignore();getline(cin,name);
+    int l = locate(name);
 }
 
-void MyClass::del(string n){
-    NODE *p, *q;
-    p=q=head;
-    if (isEmpty()){
-        cout<<"List is empty."<<endl;
-        system("pause");
-    }else {
-        while (p!=NULL && (n!=p->name)){
-            q=p;
-            p=p->next;
-        }
-        if (p==NULL){
-            printf("Not found.\n");
-            system("pause");
-        }else{
-            if (head==p)
-               head=p->next;
-            else
-               q->next = p->next;
-            free(p);
-            cout<<endl<<"Student "<<n<<" is successfuly deleted."<<endl;
-            system("pause");
-        }
-     }
+int MyClass::locate(string n){
+
 }
 
 bool MyClass::isEmpty(){
     return(head==NULL);
+}
+
+void TitleScreen(){
+    string game_title[21][98]=
+    {
+        {"##################################################################################################"},
+        {"                                                                                                  "},
+        {" ..:::::..::..:::::..::....:::::..:::::..:::::..::..:::::..::........:::::..:::::....:::......::: "},
+        {" :::'###::::'########::'####:'########:'##::::'##:'##::::'##:'########:'########:'####::'######:: "},
+        {" ::'## ##::: ##.... ##:. ##::... ##..:: ##:::: ##: ###::'###: ##.....::... ##..::. ##::'##... ##: "},
+        {" :'##:. ##:: ##:::: ##:: ##::::: ##:::: ##:::: ##: ####'####: ##:::::::::: ##::::: ##:: ##:::..:: "},
+        {" '##:::. ##: ########::: ##::::: ##:::: #########: ## ### ##: ######:::::: ##::::: ##:: ##::::::: "},
+        {" .#########: ##.. ##:::: ##::::: ##:::: ##.... ##: ##. #: ##: ##...::::::: ##::::: ##:: ##::::::: "},
+        {" .##.... ##: ##::. ##::: ##::::: ##:::: ##:::: ##: ##:.:: ##: ##:::::::::: ##::::: ##:: ##::: ##: "},
+        {" .##:::: ##: ##:::. ##:'####:::: ##:::: ##:::: ##: ##:::: ##: ########:::: ##::::'####:. ######:: "},
+        {"                             :.::::::.:...::::::.::..:::::..::........::                          "},
+        {"                             :'######::::::'###::::'##::::'##:'########:                          "},
+        {"                             '##... ##::::'## ##::: ###::'###: ##.....::                          "},
+        {"                             .##:::..::::'##:. ##:: ####'####: ##:::::::                          "},
+        {"                             .##::'####:'##:::. ##: ## ### ##: ######:::                          "},
+        {"                             .##::: ##:: #########: ##. #: ##: ##...::::                          "},
+        {"                             .##::: ##:: ##.... ##: ##:.:: ##: ##:::::::                          "},
+        {"                             ..######::: ##:::: ##: ##:::: ##: ########:                          "},
+        {"                             :......::::..:::::..::..:::::..::........::                          "},
+        {"                                                                                                  "},
+        {"##################################################################################################"}
+    };
+    for (int x=0; x<21; x++) // for looop for printing the title screen
+    {
+        cout <<"\t";
+        for (int y=0; y<98; y++)
+        {
+            cout <<game_title[x][y];
+        }
+        cout <<endl;
+        system("COLOR 0A");
+    }
+    gotoxy(40,25);system("pause");
+    system("cls");
+}
+
+int menu(){
+    int num;
+    system("cls");
+    cout <<"Arithmetic Practice Game"<<endl;
+    cout <<"=============MENU============="<<endl;
+    cout <<"Press [1] for Addition"<<endl;
+    cout <<"Press [2] for Subtraction"<<endl;
+    cout <<"Press [3] for Division"<<endl;
+    cout <<"Press [4] for Multiplication"<<endl;
+    cout <<"Press [5] to Save and Exit"<<endl;
+    cout <<"================================="<<endl;
+    cout <<"What operation do you want to take? ";
+    cin >> num;
+    return num;
 }
 
 void MyClass::display(){
@@ -154,22 +165,17 @@ void MyClass::retrieve(){
 
 int main(){
     MyClass obj;//Object
-    string Name;
-    int num,Q1,Q2,Q3;
+    srand(time(NULL));
     obj.init();
     obj.retrieve();
+    obj.logIn();
+    system("cls");
+    TitleScreen();
+    obj.leaderboard();
     while (true){
-        num = menu();
-        switch(num){
-        case 1: system("cls");
-                cout <<"Name: ";cin.ignore();getline(cin,Name);
-                cout <<"Quiz 1: ";cin >>Q1;
-                cout <<"Quiz 2: ";cin >>Q2;
-                cout <<"Quiz 3: ";cin >>Q3;
-                obj.add(Name,Q1,Q2,Q3);break;
-        case 2: system("cls");
-                cout <<"Name: ";cin.ignore();getline(cin,Name);
-                obj.del(Name);break;
+        switch(menu()){
+        case 1: system("cls"); break;
+        case 2: system("cls"); break;
         case 3: obj.display();break;
         case 4: obj.save();exit(0);
         default:cout <<"1 to 4 only."<<endl;system("pause");
